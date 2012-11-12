f07f7cc32e31c70bd3f16598eb1ee1e33a03befb
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-13 00:07:39 +0100

    Omniauth Added

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -206,7 +206,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-
+  config.omniauth :twitter, '6jaqMt7kE9iVxh8hX0mmtQ', '5ZIydxRBFfEJxc2EzedahIxoeO8g1hZCQVqZFtWWNM'
+  #config.omniauth :twitter, ENV["6jaqMt7kE9iVxh8hX0mmtQ"], ENV["5ZIydxRBFfEJxc2EzedahIxoeO8g1hZCQVqZFtWWNM"]
+   
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MoltoSoldi::Application.routes.draw do
   
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+                       controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
