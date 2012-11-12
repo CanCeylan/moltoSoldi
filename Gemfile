61da7111c7d72422672d05d0d384c2f1e1543e4d
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-12 23:35:26 +0100

    Devise Installed

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
+gem 'devise'
+gem 'omniauth-twitter'
 gem 'bootstrap-sass', '2.0.4'
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MoltoSoldi::Application.routes.draw do
   
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
