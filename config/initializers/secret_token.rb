0f30ac1259674e4d909236e0aac9974d14de7bd2
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-12 22:39:28 +0100

    Buona Sera

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoltoSoldi::Application.config.secret_token = '02df4068ebf066272951c98f5048e2c36d419535711fb710b8c3a903391de567a58d3f34750b0d0548653ee8229bfe845a3001daeb34c665ec3d2b41c4dbce30'
