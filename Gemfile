fef38469fdf843d14d195dbf8e3c856b495c9c57
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-12 23:06:41 +0100

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
+gem 'bootstrap-sass', '2.0.4'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MoltoSoldi::Application.routes.draw do
+  
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
   get "static_pages/home"
   get "static_pages/about"
   get "static_pages/help"
