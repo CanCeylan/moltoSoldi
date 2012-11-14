95f460068913f8c0269d2f8af5586cc0383077aa
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-14 20:17:46 +0100

    Controller Errors are fixed.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    if current_user
      @transactions = current_user.transactions.paginate(page: params[:page], per_page: 4)
-      @transactions_by_date = @transactions.group_by(&:deadline)
-      @date = params[:date] ? Date.parse(params[:date]) : Date.today
+     @transactions_by_date = @transactions.group_by(&:deadline)
+     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+   end
   end
 
   def help
