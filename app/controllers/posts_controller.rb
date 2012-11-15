97170a281b019eceb695eed8d224d5c660238063
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-15 03:21:49 +0100

    twitter2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
     if params[:post_type] == "twitter"
       if twitter = current_user.authentications.find_by_provider("twitter")
         Twitter.configure do |tw|
-          tw.consumer_key = "6jaqMt7kE9iVxh8hX0mmtQ"
-          tw.consumer_secret = "5ZIydxRBFfEJxc2EzedahIxoeO8g1hZCQVqZFtWWNM"
+          tw.consumer_key = "IKKOT98ZE5iJpLbQrno1Og"
+          tw.consumer_secret = "mVkcL3B95tKXYS2VjwEJruvpIBB39pUUw7j7vsj8"
           tw.oauth_token = twitter.token
           tw.oauth_token_secret = twitter.token_secret
         end
