e84ba47e165ec37dd9241b8d6618a95a6357876b
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-15 22:25:52 +0100

    Test 1

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,10 +3,17 @@ class Transaction < ActiveRecord::Base
   
   belongs_to :user
   
-  validates :amount, :presence => true
+  validates :amount, :numericality => { :greater_than_or_equal_to => 0}, :presence => true
   validates :friend_id, :presence => true
-  validates :deadline, :presence => true
+  validates :deadline,  :presence => true
+  validate  :larger_than_today
   
   TRANSACTION_TYPES = [['Borrow', '1'], ['Lend', '2']]
   TRANSACTION_SITS = [['Open', '0'], ['Close', '1']]
+  
+  def larger_than_today
+    if self.deadline < Date.today
+      errors.add :deadline, " should be bigger than today."
+    end
+  end
 end
