228051163a0ba9b52e4b50f9f24e9be51f37bc99
Orhan Can Ceylan <orhancanceylan@gmail.com>
2012-11-15 21:04:12 +0100

    heroku mail settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,12 +35,16 @@ MoltoSoldi::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+  config.action_mailer.default_url_options = { :host => 'moltosoldi.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "orhancanceylan.com",
+    domain: "moltosoldi.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "moltosoldiapp@gmail.com",
